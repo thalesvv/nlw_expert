51ef6361f28f0be3f8457ad02456b2e4f8adce82
thalesvv <thales.v.vieira@gmail.com>
2024-02-08 22:01:02 -0300

    addProduct e produto

---
+++ nlw-expert-rn/src/stores/cart-store.ts
@@ -0,0 +1,22 @@
+import { Product } from "@/components/product";
+
+import { ProductProps } from "@/utils/data/products";
+import { create } from "zustand";
+import * as cartInMemory from "./helpers/cart-in-memory";
+
+export type ProductCartProps = ProductProps & {
+  quantity: number;
+};
+
+type StateProps = {
+  products: ProductCartProps[];
+  add: (Product: ProductProps) => void;
+};
+
+export const useCartStore = create<StateProps>((set) => ({
+  products: [],
+  add: (product: ProductProps) =>
+    set((state) => ({
+      products: cartInMemory.add(state.products, product),
+    })),
+}));
